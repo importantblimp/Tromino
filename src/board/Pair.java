b3a20a05df31cf188b2fa95c84d5ff91948e98ef
kxmo <kxmo@users.noreply.github.com>
2017-12-16 15:16:39 +1300

    Add documentation to Pair

--- src/board/Pair.java
+++ src/board/Pair.java
@@ -1,5 +1,12 @@
 package board;
 
+/**
+ * A generic class for pairs of things.
+ * There are two things, a left and a right, both are final and should not be mutable.
+ * 
+ * @param <T> The left element.
+ * @param <V> The right element.
+ */
 public class Pair<T,V>
 {
 	private final T left;
@@ -21,6 +28,9 @@ public class Pair<T,V>
 		return right;
 	}
 	
+	/**
+	 * @return True iff the left of both pairs are equal and the right of both pairs are equal.
+	 */
 	@Override
 	public boolean equals(Object o)
 	{
